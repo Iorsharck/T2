c1baae397e8f4cfc8b5632ee577cb935902e3c79
Iorsharck <56987309+Iorsharck@users.noreply.github.com>
2026-03-12 21:59:28 -0600

    Update sniffer.h

--- sniffer.h
+++ sniffer.h
@@ -1,11 +1,9 @@
 #ifndef SNIFFER_H
 #define SNIFFER_H
 
-#include <pcap.h>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
-#include <chrono>
 #include "logger.h"
 
 class Sniffer
@@ -13,31 +11,24 @@ class Sniffer
 private:
     std::string interfaceName;
     int captureBytes;
-    pcap_t* handle;
+    int rawSocket;
     JSONLogger& logger;
 
     struct SynTracker
     {
         std::unordered_set<std::string> dstIPs;
         std::unordered_set<int> dstPorts;
-        std::chrono::steady_clock::time_point lastSeen;
     };
 
-    std::unordered_map<std::string, int> arpCounter;
-    std::unordered_map<std::string, SynTracker> synTracker;
+    std::unordered_map<std::string,int> arpCounter;
+    std::unordered_map<std::string,SynTracker> synTracker;
 
-    std::chrono::steady_clock::time_point lastReset;
-
-    static void packetHandler(u_char *user,
-                              const struct pcap_pkthdr *header,
-                              const u_char *packet);
-
-    void processPacket(const struct pcap_pkthdr *header, const u_char *packet);
+    void processPacket(unsigned char* buffer, int size);
     void detectARP(const std::string& srcIP);
-    void detectSYN(const std::string& srcIP, const std::string& dstIP, int dstPort);
+    void detectSYN(const std::string& srcIP,const std::string& dstIP,int port);
 
 public:
-    Sniffer(const std::string& iface, int bytes, JSONLogger& logger);
+    Sniffer(const std::string& iface,int bytes,JSONLogger& log);
     ~Sniffer();
 
     void startSniffing();
