f1380300ef185609bf1a7782dd91f4e8d22476b4
Iorsharck <56987309+Iorsharck@users.noreply.github.com>
2026-03-12 21:58:06 -0600

    Update logger.cpp

--- logger.cpp
+++ logger.cpp
@@ -1,6 +1,6 @@
 #include "logger.h"
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 using json = nlohmann::json;
 
@@ -15,67 +15,46 @@ void JSONLogger::logJSON(const std::string& input_JSON)
 
     try
     {
-        // Parse input JSON
         json input = json::parse(input_JSON);
 
-        std::cout << "Received JSON:" << std::endl;
         std::cout << input.dump(4) << std::endl;
 
         json logData;
 
-        std::ifstream inFile(logFile);
+        std::ifstream in(logFile);
 
-        if(inFile.good())
+        if(in.good())
         {
             try
             {
-                inFile >> logData;
+                in >> logData;
             }
             catch(...)
             {
-                std::cerr << "Warning: logs_JSON corrupted. Reinitializing." << std::endl;
                 logData = json::array();
             }
         }
         else
         {
-            if(errno == EACCES)
-            {
-                std::cerr << "Error: Permission denied reading log file." << std::endl;
-                return;
-            }
-
             logData = json::array();
         }
 
-        inFile.close();
-
-        if(!logData.is_array())
-        {
-            std::cerr << "Error: log file does not contain JSON array." << std::endl;
-            return;
-        }
+        in.close();
 
         logData.push_back(input);
 
-        std::ofstream outFile(logFile);
+        std::ofstream out(logFile);
 
-        if(!outFile)
+        if(!out)
         {
-            std::cerr << "Error: Cannot write to log file (permissions?)." << std::endl;
+            std::cerr << "Error writing log file\n";
             return;
         }
 
-        outFile << logData.dump(4);
-
-        outFile.close();
-    }
-    catch(const json::parse_error& e)
-    {
-        std::cerr << "Error: Invalid JSON input -> " << e.what() << std::endl;
+        out << logData.dump(4);
     }
     catch(...)
     {
-        std::cerr << "Unexpected error while logging JSON." << std::endl;
+        std::cerr << "Invalid JSON input\n";
     }
 }
